cb672de146db235e6e49bace83ed3584b8e9d13c
Niels Lindberg <eighthourcream@gmail.com>
2016-10-17 16:56:01 +0200

    function calling on inherited classes takes priority similar to javascript prototype chain

--- inheritance.py
+++ inheritance.py
@@ -4,6 +4,10 @@ class Parent():
         self.last_name = last_name
         self.eye_color = eye_color
 
+    def show_info(self):
+        print("Last Name - " + self.last_name)
+        print("Eye Color - " + self.eye_color)
+
 
 class Child(Parent):
     def __init__(self, last_name, eye_color, number_of_toys):
@@ -11,7 +15,13 @@ class Child(Parent):
         Parent.__init__(self, last_name, eye_color)
         self.number_of_toys = number_of_toys
 
+    def show_info(self):
+        print("Last Name - " + self.last_name)
+        print("Eye Color - " + self.eye_color)
+        print("Number of toys - " + str(self.number_of_toys))
+
 
 miley_cyrus = Child("Cyrus", "Blue", 5)
 print(miley_cyrus.last_name)
 print(miley_cyrus.number_of_toys)
+miley_cyrus.show_info()
